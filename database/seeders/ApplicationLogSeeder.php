548a789ed3b3c369d73aea14f1658803374d410f
Ibrahim I Musabah <i.musabah92@gmail.com>
2024-01-08 14:28:57 +0800

    Add factories and seeder

---
+++ database/seeders/ApplicationLogSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ApplicationLogSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
