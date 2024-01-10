5c3b174904dea8e7fabe03ac815ef6a31f7476f4
Ibrahim I Musabah <i.musabah92@gmail.com>
2024-01-10 10:20:41 +0800

    add models

---
+++ app/Models/OfficerUser.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OfficerUser extends User
+{
+    public static function boot() {
+        parent::boot();
+
+        static::addGlobalScope('role', function (Builder $builder) {
+            $builder->where('role',  'supervisor');
+        });
+    }
+
+}
