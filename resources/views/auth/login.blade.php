825093d70e3556c1a70a50335448a083920557c3
Ibrahim I Musabah <i.musabah92@gmail.com>
2024-01-14 12:10:37 +0800

    fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,9 +7,9 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-input-label for="metric_no" :value="__('METRIC NUMBER')" />
+            <x-text-input id="metric_no" class="block mt-1 w-full" type="text" name="metric_no" :value="old('metric_no')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('metric_no')" class="mt-2" />
         </div>
 
         <!-- Password -->
