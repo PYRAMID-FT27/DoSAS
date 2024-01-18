97e3b77bff9beb1830fe315f95eaa432f3460d3e
Ibrahim I Musabah <i.musabah92@gmail.com>
2024-01-18 13:50:58 +0800

    Fix logo style

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,2 +1,2 @@
-<img width="150" height="150" alt="utm logo" src="{{\Illuminate\Support\Facades\Storage::url('images/utm.png')}}" />
+<img class="h-full w-full" alt="utm logo" src="{{\Illuminate\Support\Facades\Storage::url('images/utm.png')}}" />
 
