9393e5757b99abe8381d389f98dc7fd2bb1d11b5
Ibrahim I Musabah <i.musabah92@gmail.com>
2024-01-28 15:28:30 +0800

    fix

--- routes/auth.php
+++ routes/auth.php
@@ -30,7 +30,7 @@ Route::middleware('guest')->group(function () {
                 ->name('password.store');
 });
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth:faculty,web,staff')->group(function () {
     Route::get('verify-email', EmailVerificationPromptController::class)
                 ->name('verification.notice');
 
